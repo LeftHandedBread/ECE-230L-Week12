1b1d5a282131568e57b5945b6aa297a5c7aca111
Bread <93460929+LeftHandedBread@users.noreply.github.com>
2025-11-11 10:38:53 -0700

    Initial commit

---
+++ onehot.v
@@ -0,0 +1,59 @@
+module one_hot(
+    input w,
+    input clk,
+    input reset,
+    output z,
+    output Astate, Bstate, Cstate, Dstate, Estate
+);
+    wire Anext, Bnext, Cnext, Dnext, Enext;
+    
+
+    dff Adff(
+        .Default(1'b1),
+        .D(Anext),
+        .clk(clk),
+        .Q(Astate),
+        .reset(reset)
+    );
+
+    dff Bdff(
+        .Default(1'b0),
+        .D(Bnext),
+        .clk(clk),
+        .Q(Bstate),
+        .reset(reset)
+    );
+
+    dff Cdff(
+        .Default(1'b0),
+        .D(Cnext),
+        .clk(clk),
+        .Q(Cstate),
+        .reset(reset)
+    );
+    
+    dff Ddff(
+        .Default(1'b0),
+        .D(Dnext),
+        .clk(clk),
+        .Q(Dstate),
+        .reset(reset)
+    );
+    
+    dff Edff(
+        .Default(1'b0),
+        .D(Enext),
+        .clk(clk),
+        .Q(Estate),
+        .reset(reset)
+    );
+
+    assign z = Cstate | Estate;
+
+    assign Anext = 1'b0;
+    assign Bnext = (~w & Astate) | (~w & Dstate) | (~w & Estate);
+    assign Cnext = (~w & Bstate) | (~w & Cstate);
+    assign Dnext = (w & Astate) | (w & Bstate) | (w & Cstate);
+    assign Enext = (w & Dstate) | (w & Estate);
+endmodule
+
---
+++ top.v
@@ -0,0 +1,30 @@
+module top(
+    input sw, // w
+    output [9:0] led, // see IO table
+    input btnC, // clk
+    input btnU // reset
+);
+
+    // Hook up binary and one-hot state machines
+    
+    one_hot c1(
+        .w(sw),
+        .clk(btnC),
+        .z(led[0]),
+        .Astate(led[2]),
+        .Bstate(led[3]),
+        .Cstate(led[4]),
+        .Dstate(led[5]),
+        .Estate(led[6]),
+        .reset(btnU)
+    );
+    
+    binary c2(
+        .w(sw),
+        .clk(btnC),
+        .z(led[1]),
+        .State(led[9:7]),
+        .reset(btnU)
+    );
+
+endmodule
